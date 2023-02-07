4678a00fe1aaa7a20adc606cf85a8970a82e7b1a
Maddy798 <hardiktaneja838@gmail.com>
2023-02-07 15:47:37 +0530

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5bYBqPni9Y7VRnnnjE-qk217q8_Vnkt8",
+  authDomain: "chuchi-afa4b.firebaseapp.com",
+  projectId: "chuchi-afa4b",
+  storageBucket: "chuchi-afa4b.appspot.com",
+  messagingSenderId: "458677175659",
+  appId: "1:458677175659:web:30f362f533589dca35782e",
+  measurementId: "G-6Q6PPEBN0S",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
