a7f80a41cbc4340b65213dc0f4e04f55575c5af9
Maddy798 <hardiktaneja838@gmail.com>
2023-02-07 16:11:09 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
-import Home from './Pages/Home.jsx'
-import Login from './Pages/Login.jsx'
-import Register from './Pages/Register.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import Login from "./Pages/Login.jsx";
+import Register from "./Pages/Register.jsx";
+import ChatPage from "./Pages/ChatPage.jsx";
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </Router>
-  )
+  );
 };
 
 export default App;
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,10 +1,37 @@
+import React, { useState } from "react";
+import GoogleButton from "react-google-button";
 
-const Register = ()=>{
-    return(
-        <div>
-            <h1>Register</h1>
-        </div>
-    )
-}
+const Register = () => {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [profilePic, setprofilePic] = useState(null);
 
-export default Register
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Username"
+        value={name}
+        onChange={(e) => setname(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setemail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setpassword(e.target.value)}
+      />
+      <input type="file" onChange={(e) => setprofilePic(e.target.files[0])} />
+      <button type="submit">Register</button>
+      <GoogleButton />
+    </form>
+  );
+};
+
+export default Register;
