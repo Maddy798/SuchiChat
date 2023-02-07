8063d834105e47409fe80fab2906195191b957ea
Schwitz <schwitz6969@gmail.com>
2023-02-07 15:51:53 +0530

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import Login from './Pages/Login.jsx'
+import Register from './Pages/Register.jsx'
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
